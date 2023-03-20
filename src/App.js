c7761534aff2bb8d88d0aa5f0687783cc8b45158
Igor Ko <ihqrk@pm.me>
2023-03-21 00:26:41 +0300

    te

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import './App.css';
-import {useEffect} from "react";
-import {useTelegram} from "./hooks/useTelegram";
-import Header from "./components/Header/Header";
-import {Route, Routes} from 'react-router-dom'
-import ProductList from "./components/ProductList/ProductList";
-import Form from "./components/Form/Form";
+import { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import { useTelegram } from "./hooks/useTelegram";
+import "./App.css";
+import Post from "./components/Post";
 
 function App() {
-    const {onToggleButton, tg} = useTelegram();
+  const { tg } = useTelegram();
 
-    useEffect(() => {
-        tg.ready();
-    }, [])
+  useEffect(() => {
+    tg.ready();
+  }, []);
 
-    return (
-        <div className="App">
-            <Header />
-            <Routes>
-                <Route index element={<ProductList />}/>
-                <Route path={'form'} element={<Form />}/>
-            </Routes>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="*" element={<Post />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Post/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import s from "./Post.module.css";
+
+const Post = () => {
+  return (
+    <div className={s.post}>
+      <img
+        className={s.image}
+        src="https://artpostergallery.ru/userdata/image/thumbs/12/76/1276d540c94d37c5d0d95df24513d30f_2.jpg"
+        alt="photo post"
+      />
+      <div className={s.content}>
+        <div className={s.title}>
+          <span>Lorem ipsum dolor sit amet, consectetur</span>
+        </div>
+        <div className={s.userBlock}>
+          <img
+            className={s.userPhoto}
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRII9U5dLvAgc70-YWpb-YbiGVvP4ptKjPekg&usqp=CAU"
+            alt="user photo"
+          />
+          <div className={s.infoUser}>
+            <span className={s.userName}>{user?.username}</span>
+            <span className={s.time}>2m ago</span>
+          </div>
+        </div>
+        <article className={s.article}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+          consectetur maximus magna, quis porta turpis venenatis consectetur.
+          Aliquam erat volutpat. Vivamus porttitor eros vel purus cursus semper.
+          Quisque ac justo dignissim, tempus leo at, consectetur ligula.
+          Praesent pulvinar urna sagittis consectetur posuere. Curabitur semper
+          metus sed elit vestibulum suscipit. Aenean vel odio urna. Quisque
+          gravida, felis nec laoreet elementum, risus diam commodo lacus, sed
+          fermentum felis dui at justo. Ut et erat posuere, consequat enim sit
+          amet, hendrerit elit. Vestibulum ante ipsum primis in faucibus orci
+          luctus et ultrices posuere cubilia curae;
+        </article>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
